88d9f2a638a918542a124afbc4de2d8f4c877b22
danielle-jones <danielle.jones67@yahoo.com>
2021-01-30 21:17:05 -0600

    First commit

---
+++ Assets/TextTransfer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public string theName;
+    public GameObject inputField;
+    public GameObject textDisplay;
+
+    public void StoreName()
+    {
+        theName = inputField.GetComponent<Text>().text;
+        textDisplay.GetComponent<Text>().text = "Welcome" + theName + " to the game";
+    }
+}
